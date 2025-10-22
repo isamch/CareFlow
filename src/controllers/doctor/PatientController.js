cee93f63407e1cfbecfce70a1cfe7f9860c38666
isam chajia <isamchajiabeat@gmail.com>
2025-10-22 14:19:30 +0100

    gestion pateint and record by doctor

--- src/controllers/doctor/PatientController.js
+++ src/controllers/doctor/PatientController.js
@@ -1,51 +1,39 @@
-import PatientRecord from './../../models/PatientRecord.js'
-import Patient from './../../models/Patient.js'
-import { successResponse } from './../../utils/apiResponse.js'
-import * as ApiError from './../../utils/ApiError.js'
-import asyncHandler from './../../utils/asyncHandler.js'
+import PatientRecord from '../../models/PatientRecord.js'
+import Patient from '../../models/Patient.js'
+import { successResponse } from '../../utils/apiResponse.js'
+import * as ApiError from '../../utils/ApiError.js'
+import asyncHandler from '../../utils/asyncHandler.js'
 
 
-
+// @desc    Doctor gets a patient's record
 export const getPatientRecord = asyncHandler(async (req, res, next) => {
-  const { id } = req.params // Patient Profile ID
-
-  const patient = await Patient.findById(id).populate({
-    path: 'patientRecord',
-    populate: [
-      {
-        path: 'visits.doctorId', select: 'specialization',
-        populate: { path: 'userId', select: 'fullName email' }
-      },
-      {
-        path: 'visits.nurseId',
-        populate: { path: 'userId', select: 'fullName email' }
-      },
-    ]
-  })
-
+  const { patientId } = req.params // This is Patient Profile ID
+  const patient = await Patient.findById(patientId).populate('patientRecord')
   if (!patient || !patient.patientRecord) {
     return next(ApiError.notFound('Patient record not found'))
   }
-
   return successResponse(res, 200, 'Patient record retrieved', patient.patientRecord)
 })
 
 
-export const addVisit = asyncHandler(async (req, res, next) => {
-  const { id } = req.params // Patient Profile ID
-  const visitData = req.body
 
-  const patient = await Patient.findById(id)
+// @desc    Doctor adds a visit
+export const addVisit = asyncHandler(async (req, res, next) => {
+  const { patientId } = req.params
+  const visitData = req.body
+  const doctorId = req.user.profileId // Doctor's Profile ID
+
+  const patient = await Patient.findById(patientId)
   if (!patient) return next(ApiError.notFound('Patient not found'))
 
   const record = await PatientRecord.findById(patient.patientRecord)
   if (!record) return next(ApiError.notFound('Patient record not found'))
 
-  visitData.doctorId = req.user.profileId // Doctor's Profile ID from JWT
-
+  visitData.doctorId = doctorId // Add the Doctor's ID to the visit
   record.visits.push(visitData)
   await record.save()
 
-  const newVisit = record.visits[record.visits.length - 1]
-  return successResponse(res, 201, 'Visit added successfully', newVisit)
+  // Return the newly added visit 
+  // pop the last visit from the visits array
+  return successResponse(res, 201, 'Visit added successfully', record.visits.pop())
 })
