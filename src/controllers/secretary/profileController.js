df80f306058f2b81dca10b472040f0f369714ace
isam chajia <isamchajiabeat@gmail.com>
2025-10-23 09:49:38 +0100

    done with sec profile

--- src/controllers/secretary/profileController.js
+++ src/controllers/secretary/profileController.js
@@ -4,6 +4,9 @@ import * as ApiError from './../../utils/ApiError.js'
 import asyncHandler from './../../utils/asyncHandler.js'
 
 
+// @desc get secretary profile by ID
+// @route GET /api/v1/secretary/profile/:id
+// @access Private (Secretary)
 export const getSecretaryProfile = asyncHandler(async (req, res, next) => {
   const { id } = req.params // Secretary Profile ID
 
@@ -18,4 +21,24 @@ export const getSecretaryProfile = asyncHandler(async (req, res, next) => {
   if (!secretary) return next(ApiError.notFound('Secretary not found'))
 
   return successResponse(res, 200, 'Secretary profile retrieved', secretary)
+})
+
+
+
+
+// @desc update my profile
+// @route PUT /api/v1/secretary/profile/me
+// @access Private (Secretary)
+export const updateMyProfile = asyncHandler(async (req, res, next) => {
+  const profileId = req.user.profileId
+  const { managingDoctors } = req.body
+  
+  const profile = await Secretary.findByIdAndUpdate(
+    profileId, 
+    { managingDoctors },
+    { new: true, runValidators: true }
+  )
+  
+  if (!profile) return next(ApiError.notFound('Secretary profile not found'))
+  return successResponse(res, 200, 'Profile updated', profile)
 })
